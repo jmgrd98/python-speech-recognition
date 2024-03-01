962a5be51d6a01e6226ab69cccef9f1e61d05620
João Marcelo Dantas <jmgrd98@gmail.com>
2024-03-01 10:44:34 -0300

    commit

--- index.py
+++ index.py
@@ -1,16 +1,38 @@
+import pyaudio
+import wave
 import speech_recognition as sr
 
-rec = sr.Recognizer()
+FRAMES_PER_BUFFER = 3200
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 16000
 
-# print(sr.Microphone().list_microphone_names())
-with sr.Microphone(1) as mic:
-    rec.adjust_for_ambient_noise(mic)
-    print('Fale agora')
-    audio = rec.listen(mic)
-    try:
-        text = rec.recognize_google(audio, language='pt-BR')
-        print(text)
-    except sr.UnknownValueError:
-        print("Não foi possível entender o áudio")
-    except sr.RequestError as e:
-        print(f"Erro ao fazer a requisição ao Google Speech Recognition service; {e}")
+p = pyaudio.PyAudio()
+
+stream = p.open(
+    format=FORMAT,
+    channels=CHANNELS,
+    rate=RATE,
+    input=True,
+    frames_per_buffer=FRAMES_PER_BUFFER,
+)
+
+print('Start recording')
+
+seconds = 5
+frames = []
+
+for i in range(0, int(RATE /  FRAMES_PER_BUFFER * seconds)):
+    data = stream.read(FRAMES_PER_BUFFER)
+    frames.append(data)
+    
+stream.stop_stream()
+stream.close()
+p.terminate()
+
+obj = wave.open('output.wav', 'wb')
+obj.setnchannels(CHANNELS)
+obj.setsampwidth(p.get_sample_size(FORMAT))
+obj.setframerate(RATE)
+obj.writeframes(b"".join(frames))
+obj.close()
